ec510533c6f3587318822aaeed20a261256d56b9
Dongseok Shim <tlaehdtjr01@snu.ac.kr>
2020-06-19 16:15:48 +0900

    Add files via upload

---
+++ dataloader.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+import torch
+from skimage import io
+from torchvision import transforms
+from PIL import Image
+import pandas as pd
+
+class MnistClutteredDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, type, transform=None):
+
+        self.root_dir = '/media/dsshim/mnist_distortion/' + type + '/path.txt'
+        self.transform = transform
+        self.path = pd.read_csv(self.root_dir, sep=' ', header=None)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = self.path.iloc[idx,0]
+
+        image = Image.open(img_path)
+
+        label = int(self.path.iloc[idx,1])
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+    def __len__(self):
+        return len(self.path)
